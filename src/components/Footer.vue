c64b29129feb4eb158efbf1ff0c7f249bfb4762b
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-08-21 14:23:18 +0700

    Add footer and change web icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Ini Footer</h1>
+  <footer class="page-footer text-light footer fixed-bottom">
+    <div class="container">
+      <p class="footer-subtitle text-center mt-2 mb-0">Powered by</p>
+      <h1 class="footer-title-logo text-center">HACK UGM</h1>
+      <p class="footer-subtitle text-center mb-0">Organized by</p>
+      <h1 class="footer-title text-center">BEM KM UGM</h1>
+      <p class="footer-subtitle text-center">
+        Copyright <img class="copyright" src="../assets/copyright.svg" /> 2020
+      </p>
+    </div>
+  </footer>
 </template>
