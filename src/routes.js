a606a1f65dd626b2728c0e6d1fe2166ce4cb0da8
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-08-27 17:27:55 +0700

    Fix auto scrolling top

--- src/routes.js
+++ src/routes.js
@@ -53,6 +53,13 @@ const routes = [{
 const router = new VueRouter({
   mode: "history",
   routes,
+  // eslint-disable-next-line no-unused-vars
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 });
 
 export default router;
