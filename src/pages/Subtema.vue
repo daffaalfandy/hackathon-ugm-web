e48f6685642f428b952b305bfbf3a7a9ea31dfed
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-08-29 12:25:45 +0700

    Add webp imgaes

--- src/pages/Subtema.vue
+++ src/pages/Subtema.vue
@@ -2,7 +2,15 @@
   <div class="container-fluid shadow">
     <div class="row">
       <div class="col-lg-4 col-md-5 col-sm-12">
-        <img src="../assets/education.png" alt="Education Image" class="img-theme" />
+        <picture>
+          <source srcset="../assets/education.webp" type="images/webp" />
+          <source srcset="../assets/education.png" type="images/png" />
+          <img
+            src="../assets/education.png"
+            alt="Education Image"
+            class="img-theme"
+          />
+        </picture>
       </div>
       <div class="col-lg-8 col-md-7 col-sm-12">
         <h1 class="title-theme">Education</h1>
@@ -16,7 +24,11 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-md-5 col-sm-12">
-        <img src="../assets/salth.png" alt="Salth Image" class="img-theme" />
+        <picture>
+          <source srcset="../assets/salth.webp" type="images/webp" />
+          <source srcset="../assets/salth.png" type="images/png" />
+          <img src="../assets/salth.png" alt="Salth Image" class="img-theme" />
+        </picture>
       </div>
       <div class="col-lg-8 col-md-7 col-sm-12">
         <h1 class="title-theme">Salth</h1>
@@ -31,7 +43,15 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-md-5 col-sm-12">
-        <img src="../assets/entertainment.png" alt="Entertainment Image" class="img-theme" />
+        <picture>
+          <source srcset="../assets/entertainment.webp" type="images/webp" />
+          <source srcset="../assets/entertainment.png" type="images/png" />
+          <img
+            src="../assets/entertainment.png"
+            alt="Entertainment Image"
+            class="img-theme"
+          />
+        </picture>
       </div>
       <div class="col-lg-8 col-md-7 col-sm-12">
         <h1 class="title-theme">Entertainment</h1>
@@ -45,7 +65,15 @@
     </div>
     <div class="row">
       <div class="col-lg-4 col-md-5 col-sm-12">
-        <img src="../assets/acavices.png" alt="Acavices Image" class="img-theme" />
+        <picture>
+          <source srcset="../assets/acavices.webp" type="images/webp" />
+          <source srcset="../assets/acavices.png" type="images/png" />
+          <img
+            src="../assets/acavices.png"
+            alt="Acavices Image"
+            class="img-theme"
+          />
+        </picture>
       </div>
       <div class="col-lg-8 col-md-7 col-sm-12">
         <h1 class="title-theme">Acavices</h1>
