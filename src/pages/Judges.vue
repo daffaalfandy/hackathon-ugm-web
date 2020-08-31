1911e10fbe1217d5d00624a579af17b4b449db14
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-08-31 11:21:35 +0700

    Fix judges page

--- src/pages/Judges.vue
+++ src/pages/Judges.vue
@@ -8,6 +8,21 @@
     <div class="container">
       <div class="row mb-5 text-center">
         <div class="col-lg-4 col-md-6">
+          <img src="../assets/christoper-farrel.jpeg" style="border-radius: 50%" alt="Judge Photos" />
+          <h4 class="ml-0 text-title mt-4 mb-3">Christoper Farrel Millenio Kusuma</h4>
+          <p class="text-desc">CEO Kecilin.id & Angel Investor</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <img src="../assets/hibatul-ghazi.jpeg" style="border-radius: 50%" alt="Judge Photos" />
+          <h4 class="ml-0 text-title mt-4 mb-3">Hibatul Ghazi Zulhasmi</h4>
+          <p class="text-desc">Mapres 1 UGM 2019</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <img src="../assets/fadhlan-hawali.jpeg" style="border-radius: 50%" alt="Judge Photos" />
+          <h4 class="ml-0 text-title mt-4 mb-3">Fadhlan Hawali, S.T.</h4>
+          <p class="text-desc">PM MeetingYuk</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
           <img src="../assets/ahmad-fauzi.jpeg" style="border-radius: 50%" alt="Judge Photos" />
           <h4 class="ml-0 text-title mt-4 mb-3">Ahmad Fauzi Wibowo</h4>
           <p class="text-desc">
@@ -24,24 +39,9 @@
           </p>
         </div>
         <div class="col-lg-4 col-md-6">
-          <img src="../assets/fadhlan-hawali.jpeg" style="border-radius: 50%" alt="Judge Photos" />
-          <h4 class="ml-0 text-title mt-4 mb-3">Fadhlan Hawali, S.T.</h4>
-          <p class="text-desc">PM MeetingYuk</p>
-        </div>
-        <div class="col-lg-4 col-md-6">
           <img src="../assets/wisnu-kurniawan.jpeg" style="border-radius: 50%" alt="Judge Photos" />
           <h4 class="ml-0 text-title mt-4 mb-3">Wisnu Kurniawan</h4>
           <p class="text-desc">Software Engineer Bahasa.ai</p>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <img src="../assets/christoper-farrel.jpeg" style="border-radius: 50%" alt="Judge Photos" />
-          <h4 class="ml-0 text-title mt-4 mb-3">Christoper Farrel Millenio Kusuma</h4>
-          <p class="text-desc">CEO Kecilin.id & Angel Investor</p>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <img src="../assets/hibatul-ghazi.jpeg" style="border-radius: 50%" alt="Judge Photos" />
-          <h4 class="ml-0 text-title mt-4 mb-3">Hibatul Ghazi Zulhasmi</h4>
-          <p class="text-desc">Mapres 1 UGM 2019</p>
         </div>
       </div>
     </div>
