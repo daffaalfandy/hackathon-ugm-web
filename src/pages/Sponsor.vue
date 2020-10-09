1c77c37e985dbf24dda511e6612f80109f7a275e
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-10-09 18:50:42 +0700

    Add Sponsor Picture

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -6,8 +6,30 @@
       </div>
     </div>
     <div class="container">
-      <div class="col-12 text-center">
-        <h1 class="creativity fade-in">COMING SOON</h1>
+      <div class="row my-5 text-center">
+        <div class="col-lg-4 col-sm-6 my-3">
+          <img src="../assets/logo-1.png" alt="Sponsor Logo" />
+        </div>
+        <div class="col-lg-4 col-sm-6 my-3">
+          <img src="../assets/logo-2.png" alt="Sponsor Logo" />
+        </div>
+        <div class="col-lg-4 col-sm-6 my-3">
+          <img src="../assets/logo-3.png" alt="Sponsor Logo" />
+        </div>
+        <div class="col-lg-6 col-sm-12 my-3">
+          <img
+            src="../assets/logo-4.png"
+            class="long-logo"
+            alt="Sponsor Logo"
+          />
+        </div>
+        <div class="col-lg-6 col-sm-12 my-3">
+          <img
+            src="../assets/logo-5.png"
+            class="long-logo"
+            alt="Sponsor Logo"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -16,8 +38,8 @@
 <script>
 export default {
   mounted() {
-    document.getElementById("footer").classList.add("fixed-bottom");
-    document.getElementById("footer").classList.add("footer-mobile");
+    document.getElementById("footer").classList.remove("fixed-bottom");
+    document.getElementById("footer").classList.remove("footer-mobile");
   },
 };
 </script>
